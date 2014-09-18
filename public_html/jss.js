b9430ba83d0537b4a09a572dac92a515e50cfa15
justin munoz <jayymunoz66@gmail.com>
2014-09-18 12:28:08 -0700

    robben flops

---
+++ public_html/jss.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* 
+Maybe someone can help with the jumping back?
+Pictures from wikipedia commons:
+http://commons.wikimedia.org/wiki/File:Dawn_at_swifts_creek_panorama.jpg
+http://commons.wikimedia.org/wiki/File:Mist_-_Ensay_region3.jpg
+http://commons.wikimedia.org/wiki/File:Lower_Manhattan_from_Staten_Island_Ferry_Jan_2006.jpg
+http://commons.wikimedia.org/wiki/File:Mt_hotham_alpine_range_scenery.jpg
+http://commons.wikimedia.org/wiki/File:Sydney_Harbour_Bridge_night.jpg
+*/
+
